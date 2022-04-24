788ec3602c954564b99b7ded938510ef0305cbf6
Cerberus <cerberusprogrammer@gmail.com>
2022-04-24 12:58:50 -0700

    New animation :D

--- src/main/java/com/example/materialnotes/App.java
+++ src/main/java/com/example/materialnotes/App.java
@@ -1,9 +1,15 @@
 package com.example.materialnotes;
 
+import javafx.animation.TranslateTransition;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.SplitPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.Circle;
+import javafx.util.Duration;
 
 
 import java.net.URL;
@@ -11,8 +17,15 @@ import java.util.ResourceBundle;
 
 public class App implements Initializable {
 
+    public Circle circle;
     @FXML
     private HBox bottomBar;
+    @FXML
+    private ScrollPane createPane;
+    @FXML
+    private ScrollPane notesPane;
+    @FXML
+    private ScrollPane searchPane;
 
     @FXML
     void createNote(ActionEvent event) {
@@ -29,8 +42,41 @@ public class App implements Initializable {
 
     }
 
+    @FXML
+    void displayNotes(ActionEvent event) {
+        TranslateTransition search = new TranslateTransition(new Duration(250), searchPane);
+        TranslateTransition create = new TranslateTransition(new Duration(250), createPane);
+        create.setToX(10);
+        search.setToX(10);
+        create.play();
+        search.play();
+    }
+
+    @FXML
+    void displaySearch(ActionEvent event) {
+        TranslateTransition search = new TranslateTransition(new Duration(250), searchPane);
+        TranslateTransition create = new TranslateTransition(new Duration(250), createPane);
+        create.setToX(0);
+        search.setToX(-498);
+        create.play();
+        search.play();
+    }
+
+    @FXML
+    void displayCreateNote(ActionEvent event) {
+        TranslateTransition create = new TranslateTransition(new Duration(250), createPane);
+        TranslateTransition search = new TranslateTransition(new Duration(250), searchPane);
+        search.setToX(-498);
+        create.setToX(-498);
+        search.play();
+        create.play();
+    }
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-
+        TranslateTransition search = new TranslateTransition(new Duration(250), searchPane);
+        TranslateTransition create = new TranslateTransition(new Duration(250), createPane);
+        search.setToX(10);
+        create.setToX(10);
     }
 }
--- src/main/java/com/example/materialnotes/Start.java
+++ src/main/java/com/example/materialnotes/Start.java
@@ -11,7 +11,7 @@ public class Start extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Start.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 500, 720);
+        Scene scene = new Scene(fxmlLoader.load(), 500, 700);
         stage.setTitle("Material Notes");
         stage.setScene(scene);
         stage.show();
@@ -22,12 +22,3 @@ public class Start extends Application {
         launch();
     }
 }
-
-/*
-<MFXCircleToggleNode text="hello" accessibleRole="BUTTON" contentDisplay="TOP" gap="0.0" graphicTextGap="1.0" layoutX="419.0" layoutY="4.0" prefHeight="42.0" prefWidth="86.0" size="20.0">
-            </MFXCircleToggleNode>
-
-
-            <MFXButton buttonType="RAISED" depthLevel="LEVEL1" rippleColor="#1fa8d9" text="Notes" />
-            <MFXButton buttonType="RAISED" depthLevel="LEVEL1" rippleColor="#1fa8d9" text="Search" />
- */
